cd883aeff49e8472e4bb5c3b55db3c717702e472
Sergiu Alexandru Antal <sergiu.antal@endava.com>
2020-06-01 19:36:27 +0300

    Add project files.

---
+++ ExcelConvert/Program.cs
@@ -0,0 +1,80 @@
+﻿using Ganss.Excel;
+using System;
+using System.Globalization;
+
+namespace ExcelConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = @"C:\Users\ECHO\Desktop\part1.xlsx";
+            var testData = new ExcelMapper(fileName).Fetch<TestData>();
+            int i = 0;
+            foreach(var data in testData)
+            {
+                i++;
+                Console.WriteLine("<dataSet_" + i+">");
+                Console.WriteLine($"<coverCyberBbr>{data.IncludeBBR}</coverCyberBbr>");
+                Console.WriteLine($"<coverDO>{data.DO}</coverDO>");
+                Console.WriteLine($"<coverEPL>{data.IncludeEPL}</coverEPL>");
+                Console.WriteLine($"<coverCrime>{data.IncludeCrime}</coverCrime>");
+                Console.WriteLine($"<primaryIndustryActivity>{data.Industry}</primaryIndustryActivity>");
+                Console.WriteLine($"<moreThan24MonthsOfOperation>FALSE</moreThan24MonthsOfOperation>");
+                Console.WriteLine($"<usaRevenuesLess25Percent>TRUE</usaRevenuesLess25Percent>");
+                Console.WriteLine($"<numberOfEmployees>{data.NoOfEmployees}</numberOfEmployees>");
+                Console.WriteLine($"<numberOfLocations>{data.NoOfLocations}</numberOfLocations>");
+                WriteBBR(data);
+                WriteDO(data);
+                WriteEPL(data);
+                WriteCrime(data);
+                Console.WriteLine("</dataSet_" + i + ">");
+                Console.WriteLine();
+            }
+        }
+
+        public static void WriteBBR(TestData data)
+        {
+            string limit = int.Parse(data.BBRLimit).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            string excess = int.Parse(data.BBRExcess).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            string notif = int.Parse(data.Notification).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            Console.WriteLine($"<cyberBbrFirstPartyLimit>{limit}</cyberBbrFirstPartyLimit>");
+            Console.WriteLine($"<cyberBbrFirstPartyRetention>{excess}</cyberBbrFirstPartyRetention>");
+            Console.WriteLine($"<cyberBbrNotificationLimit>{notif}</cyberBbrNotificationLimit>");
+            Console.WriteLine($"<cyberBbrSecurityTraining>TRUE</cyberBbrSecurityTraining>");
+            Console.WriteLine($"<cyberBbrECrime>TRUE</cyberBbrECrime>");
+        }
+
+        public static void WriteDO(TestData data)
+        {
+            string limit = int.Parse(data.DOlimit).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            string excess = int.Parse(data.DOExcess).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            Console.WriteLine($"<doLimit>{limit}</doLimit>");
+            Console.WriteLine($"<doRetention>{excess}</doRetention>");
+        }
+
+        public static void WriteEPL(TestData data)
+        {
+            string limit = string.Empty;
+            try
+            {
+               limit = int.Parse(data.EPLLimit).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            }
+            catch(Exception ex)
+            {
+                limit = data.EPLLimit;
+            }
+            string excess = int.Parse(data.EPLExcess).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            Console.WriteLine($"<eplLimit>{limit}</eplLimit>");
+            Console.WriteLine($"<eplRetention>{excess}</eplRetention>");
+        }
+
+         public static void WriteCrime(TestData data)
+        {
+            string limit = int.Parse(data.Crimelimit).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            string excess = int.Parse(data.CrimeExcess).ToString("N0", CultureInfo.CreateSpecificCulture("en-GB"));
+            Console.WriteLine($"<crimeLimit>{limit}</crimeLimit>");
+            Console.WriteLine($"<crimeRetention>{excess}</crimeRetention>");
+        }
+    }
+}
---
+++ ExcelConvert/TestData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelConvert
+{
+    public class TestData
+    {
+        public string Industry { get; set; }
+        public string DO { get; set; }
+        public string Turnover { get; set; }
+        public string DOlimit { get; set; }
+        public string DOExcess { get; set; }
+        public string IncludeEPL { get; set; }
+        public string NoOfEmployees { get; set; }
+        public string EPLLimit { get; set; }
+        public string EPLExcess { get; set; }
+        public string IncludeCrime { get; set; }
+        public string NoOfLocations { get; set; }
+        public string Crimelimit { get; set; }
+        public string CrimeExcess { get; set; }
+        public string IncludeBBR { get; set; }
+        public string BBRLimit { get; set; }
+        public string Notification { get; set; }
+        public string BBRExcess { get; set; }
+        public string TotalPremium { get; set; }
+    }
+}
